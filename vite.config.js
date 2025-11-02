584e9e9e33eb07d858ed36b4bd080b099d9ff5ec
holysmokas <holysmokasthatscheap@gmail.com>
2025-11-02 15:39:18 +0100

    Build for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/rossi-mission-sf/',  // Replace with your repo name
+  base: '/rossi-mission-sf/',
   plugins: [react()],
+  build: {
+    outDir: 'docs'  // Change output from 'dist' to 'docs'
+  }
 })
