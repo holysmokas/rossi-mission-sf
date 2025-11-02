7d5220b79592ee957e01e866dc112c1f6b186fe9
holysmokas <holysmokasthatscheap@gmail.com>
2025-11-02 22:18:09 +0100

    changed admin and shop page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
+import React from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Shop from "./Shop";
+import About from "./About";
+import Login from "./Login";
+import Admin from "./Admin";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Shop />} />
         <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
