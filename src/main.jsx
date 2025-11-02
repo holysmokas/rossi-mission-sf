f40cbe1b0c301846ac4a88b5f4b7bc02329ba6d0
holysmokas <holysmokasthatscheap@gmail.com>
2025-11-02 15:46:56 +0100

    Fix routing for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,19 +4,18 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import About from './About'
 import Admin from './Admin'
-import Login from './Login'  // <-- Add this
+import Login from './Login'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/rossi-mission-sf">  {/* Add basename here */}
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/about" element={<About />} />
-        <Route path="/login" element={<Login />} />  {/* <-- Add this */}
+        <Route path="/login" element={<Login />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
 )
-
