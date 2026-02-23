42a4525e0b8cb6e0f8e7e28340c2571da70a216c
holysmokas <holysmokasthatscheap@gmail.com>
2026-02-22 21:02:22 -0800

    website design overhaul

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { supabase } from '../supabaseClient'
+import { supabase } from '../lib/supabase'
 import Reveal from './Reveal'
 import './Newsletter.css'
 
